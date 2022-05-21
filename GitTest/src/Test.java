c16f37c1932d81b689e75213a505c8b29db216d4
rjsdn <rjsdn@PGW>
2022-05-21 18:18:35 +0900

    건우가 집에서 직접 실행해 본 깃허브

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("첫 번째 커밋!");
 		System.out.println("두 번째 커밋!!");
 		System.out.println("세 번째 커밋!!!");
+		System.out.println("깃허브 테스트, 현재 건우 노트북으로 깃허브 pull");
 		
 	}
 
